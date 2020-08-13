b2aec09eeac4895a27bebe0543ff6da6261e8e90
Jennifer Stewart <jstewart8053@gmail.com>
2020-08-13 12:24:47 -0500

    app and image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Image from './Image';
 import "./App.css";
 
+const nasaApi = "https://api.nasa.gov/planetary/apod?api_key=6s8lcv8nth7JkF1rcx3Ng8jGwvWcXbFdGb4VNbcy";
+
 function App() {
+  const [apod, setApod] = useState([]);
+
+ 
+  useEffect(() => {
+    axios.get(nasaApi)
+      .then(response => setData(response.data))
+      .catch(error => error)
+  }, [])
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     <h1> Nasa Image of the Day </h1>
     </div>
   );
 }
